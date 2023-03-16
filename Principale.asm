21f34a696cbb3c3a8cbacd4dce69f7229fe2dd08
Chanfreau <cedric.chanfreau@gmail.com>
2023-03-16 20:26:14 +0100

    Led Détecte Front Montant sans variable

--- Principale.asm
+++ Principale.asm
@@ -11,7 +11,7 @@
 
 
 ;************************************************************************
-; 					IMPORT/EXPORT Système
+; 					IMPORT/EXPORT Syst?me
 ;************************************************************************
 
 	IMPORT ||Lib$$Request$$armlib|| [CODE,WEAK]
@@ -21,7 +21,7 @@
 	;IMPORT Capteur
 
 
-; IMPORT/EXPORT de procédure           
+; IMPORT/EXPORT de proc?dure           
 
 	IMPORT Init_Cible
 	
@@ -44,7 +44,7 @@
 
 
 ;*******************************************************************************
-; Procédure principale et point d'entrée du projet
+; Proc?dure principale et point d'entr?e du projet
 ;*******************************************************************************
 main  	PROC 
 ;*******************************************************************************
@@ -76,97 +76,92 @@ main  	PROC
 			
 
 
-OldEtat		DCW		0
-;Cpt			DCB		0
-N			EQU		10
+OldEtat		DCW		0			;Variable pour stocker l'?tat pr?c?dent du capteur
+Cpt			DCB		0		;Compteur de fronts montants d?tect?s
+N			EQU		10			;Nombre de fronts montants ? d?tecter avant de rester allum?
 
 boucle
 
 
-		LDR  R4 ,= 0X40010808
-		LDRH R8, [R4]
-		AND R8,R8, #0x0100
-		CMP R8, #0
-		BEQ allume
-		MOV R7,#0x00
-		B boucle
-		
-		check
-		LDR R6, =OldEtat
-		LDRH R7,[R6]		
-		MOV R7,#0x01
-		AND R7,R7, #0x01
-		CMP R6, #0
-		BNE exstate1
-		
-
-		
-		
+		;Etat du capteur
+		LDR  R4 ,= 0X40010808		; Chargement de l'adresse dans le registre
+		LDRH R8, [R4]				; Stock la valeur de l'adresse de R4 dans R5
 		LDR R9, =N
 		LDRH R10, [R9]
-		AND R10,R10, #0x0A
-		CMP R10, #0x0A
-		BL Allume
-		
+		AND R8,R8, #0x0100			; V?rifier si le bit 8 de l'adresse charg?e dans R5 est ? 1 ou ? 0.
+		CMP R8, #0					; Compare R8 ? z?ro OU CMP R8, #(0x01 << 8)
+		BNE allume					; Si zero alors j'allume
+		MOV R7,#0x00				; 
+		B boucle
 		
 	
-	
+					
+		
 allume
 
 		BL Allume_LED
-		ADD R10, #0x01
 		
+
+		ADD R10, R10, #1
+		AND R11,R10, #0x0A
+		CMP R10, #0x0A
+		BEQ Allume
+		
+		
+		LDRH R8, [R4]				; Stock la valeur de l'adresse de R4 dans R5
 		AND R8,R8, #0x0100
 		CMP R8, #0
 		BNE eteint
 		
-		B boucle
+		B allume
 		
+		
+		
+		B boucle
 
-check
 
+	
+
+eteint
 		BL Eteint_LED
-		MOV R7,#0x01
+		
+		ADD R10, #0x01
+		AND R11,R10, #0x0A
+		CMP R11, #0x0A
+		BEQ Allume
+		
+		
+		LDRH R8, [R4]				; Stock la valeur de l'adresse de R4 dans R5
+		AND R8,R8, #0x0100
+		CMP R8, #0
+		BNE allume
+		
+		B eteint
 		
 		B boucle
-
-
 		
-
-exstate1
-		B boucle
-	
-	
-
-
-
-
-
+		
+		
+		
 boucle2
 Allume	
+		BL Allume_LED
 		B boucle2
-
-		
-		
-		
-	
-		B boucle
-
-
-
-
-
-
-
-       
-
-Eteint
-		BL Eteint_LED
-		B boucle
 		
 		
 		
 		
+		
+		
+		
+		
+;check
+;		LDR R6, =OldEtat
+;		LDRH R7,[R6]		
+;		MOV R7,#0x01
+;		AND R7,R7, #0x01
+;		CMP R6, #0
+;		BNE exstate1
 		
 		B .			 ; boucle inifinie terminale...
 
