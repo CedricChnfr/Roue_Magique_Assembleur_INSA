db8672b05c5645cca633f2be7be1d22b91791717
Chanfreau <cedric.chanfreau@gmail.com>
2023-03-18 19:23:58 +0100

    Détecte front montant Marche

--- Principale.asm
+++ Principale.asm
@@ -52,7 +52,7 @@ main  	PROC
 
 	
 	MOV R0,#0
-	BL Init_Cible;				;Initialisation de la cible
+	BL Init_Cible;					;Initialisation de la cible
 
 boucle
 
@@ -60,8 +60,7 @@ boucle
 		LDR  R4 ,= 0X40010808		; Chargement de l'adresse dans le registre
 		LDRH R8, [R4]				; Stock la valeur de l'adresse de R4 dans R8
 		
-		MOV R9, #0					; Registre du compteur des 10 passages
-		
+		MOV R9, #0					; Registre du compteur des 10 passages initialisé à 0
 		MOV R10, #0					; Registre OlEtat
 		
 		AND R8,R8, #0x0100			; Vérifier si le bit 8 de l'adresse chargée dans R5 est à 1 ou à 0.
@@ -71,57 +70,68 @@ boucle
 			
 front_montant_allume
 		
-		ADD R9, R9, #1
-		CMP R9, #0x0A
-		BEQ Allume
+		ADD R9, R9, #1				; Incrémentation du compteur des 10 passages
+		CMP R9, #0x0A				; Vérifier si le compteur est à 10
+		BEQ Allume					; Si oui, alors passer à l'étape d'allumage permanent de la LED
 		
-		BL Allume_LED
-		MOV R10, #1
+		MOV R10, #1					; Mettre à jour OldEtat
 		
+attente_allume
+		BL Allume_LED				; Sinon, allume la LED
+		BL Old_Etat_allume			; Récupérer l'état précédent du capteur
 allume
+		CMP R10, #0					; Compare le registre R10 à 0
+		BEQ front_montant_eteint	; Si oui retourne à l'étiquette front_montant_eteint
 		
-		BL Old_Etat
-allume2
-		CMP R10, #0
-		BEQ front_montant_eteint
-		
-		B allume
+		B attente_allume					; Sinon continuer l'étape d'allumage
 
 front_montant_eteint
 		
-		ADD R9, R9, #1
-		CMP R9, #0x0A
-		BEQ Allume
+		ADD R9, R9, #1				; Incrémentation du compteur des 10 passages
+		CMP R9, #0x0A				; Vérifier si le compteur est à 10
+		BEQ Allume					; Si oui, alors passer à l'étape d'allumage permanent de la LED
+
+		MOV R10, #1					; Mettre à jour OldEtat
 		
-		BL Eteint_LED
-		MOV R10, #1
+attente_eteint	
+		BL Eteint_LED				; Sinon, éteindre la LED
+		BL Old_Etat_eteint			; Récupérer l'état précédent du capteur
+eteint
+		CMP R10, #0					; Compare le registre R10 à 0
+		BEQ front_montant_allume	; Si oui retourne à l'étiquette front_montant_allume
 		
-eteint	
-		BL Old_Etat2
-		MOV R10, #1
-eteint2
-		
-		CMP R10, #0
-		BEQ front_montant_allume
-		
-		B eteint
+		B attente_eteint
 		
 		
-Old_Etat
-		MOV R10, #0
-		LDRH R8, [R4]				; Stock la valeur de l'adresse de R4 dans R5
-		AND R8,R8, #0x0100
-		CMP R8, #0
-		BEQ allume2
-		B Old_Etat
+Old_Etat_allume
+		MOV R10, #0					; Initialisation à 0 du registre OldEtat
+		LDRH R8, [R4]				; Charge la valeur stockée à l'adresse R4 dans le registre R8
+		AND R8,R8, #0x0100			; Effectue un ET binaire entre R8 et 0x0100 et stocke le résultat dans R8
+		CMP R8, #0					; Compare la valeur stockée dans R8 à 0
+		BEQ Second_check			; Saute à l'étiquette allume2
+		B Old_Etat_allume			; Sinon, retourne à l'étiquette Old_Etat_allume
+Second_check
+		LDRH R8, [R4]				; Charge la valeur stockée à l'adresse R4 dans le registre R8
+		AND R8,R8, #0x0100			; Effectue un ET binaire entre R8 et 0x0100 et stocke le résultat dans R8
+		CMP R8, #0					; Compare la valeur stockée dans R8 à 0
+		BNE allume					; Saute à l'étiquette allume2
+		B Second_check				; Sinon, retourne à l'étiquette Old_Etat_allume
 		
-Old_Etat2
-		MOV R10, #0
-		LDRH R8, [R4]				; Stock la valeur de l'adresse de R4 dans R5
-		AND R8,R8, #0x0100
-		CMP R8, #0
-		BNE eteint2
-		B Old_Etat2
+Old_Etat_eteint
+		MOV R10, #0					; Initialisation à 0 du registre OldEtat
+		LDRH R8, [R4]				; Charge la valeur stockée à l'adresse R4 dans le registre R8
+		AND R8,R8, #0x0100			; Effectue un ET binaire entre R8 et 0x0100 et stocke le résultat dans R8
+		CMP R8, #0					; Compare la valeur stockée dans R8 à 0
+		BEQ Second_check2					; Si la comparaison n'est pas égale, saute à l'étiquette eteint2
+		B Old_Etat_eteint			; Sinon, retourne à l'étiquette Old_Etat_eteint
+Second_check2
+		LDRH R8, [R4]				; Charge la valeur stockée à l'adresse R4 dans le registre R8
+		AND R8,R8, #0x0100			; Effectue un ET binaire entre R8 et 0x0100 et stocke le résultat dans R8
+		CMP R8, #0					; Compare la valeur stockée dans R8 à 0
+		BNE eteint					; Saute à l'étiquette allume2
+		B Second_check2				; Sinon, retourne à l'étiquette Old_Etat_allume
+
+
 
 boucle2
 Allume	
@@ -129,18 +139,7 @@ Allume
 		B boucle2
 		
 	
-		
-		
-		
-;check
-;		LDR R6, =OldEtat
-;		LDRH R7,[R6]		
-;		MOV R7,#0x01
-;		AND R7,R7, #0x01
-;		CMP R6, #0
-;		BNE exstate1
-		
-		B .			 ; boucle inifinie terminale...
+		B .							; boucle inifinie terminale...
 
 		ENDP
 
