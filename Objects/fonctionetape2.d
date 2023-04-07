e19237a6642159827294ec85903b4d580cea21a7
Chanfreau <cedric.chanfreau@gmail.com>
2023-04-07 22:24:45 +0200

    Ajout de .gitignore pour tt ignorer

--- Objects/fonctionetape2.d
+++
@@ -1,3 +0,0 @@
-.\objects\fonctionetape2.o: FonctionEtape2.asm
-.\objects\fonctionetape2.o: REG_UTILES.inc
-.\objects\fonctionetape2.o: Lumiere.asm
--- Objects/principale.d
+++
@@ -1,3 +0,0 @@
-.\objects\principale.o: Principale.asm
-.\objects\principale.o: REG_UTILES.inc
-.\objects\principale.o: Lumiere.asm
